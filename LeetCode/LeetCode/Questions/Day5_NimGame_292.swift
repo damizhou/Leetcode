dfbccc2901607b4234507eca6b21920d7dab5428
598069751@qq.com <598069751@qq.com>
2016-10-25 09:42:26 +0800

    第5天292. Nim Game

---
+++ LeetCode/LeetCode/Questions/Day5_NimGame_292.swift
@@ -0,0 +1,24 @@
+//
+//  Day5_NimGame_292.swift
+//  LeetCode
+//
+//  Created by 潘传洲 on 16/10/25.
+//  Copyright © 2016年 pcz. All rights reserved.
+//
+
+import Foundation
+
+class Day5_NimGame_292: NSObject {
+    
+    /// 题目和解题思路见292. NimGame.md
+    private class func canWinNim(_ n: Int) -> Bool {
+        return n % 4 != 0
+    }
+    
+    class func solution() {
+        
+        let a = 32
+        let c = self.canWinNim(a);
+        print(c)
+    }
+}
