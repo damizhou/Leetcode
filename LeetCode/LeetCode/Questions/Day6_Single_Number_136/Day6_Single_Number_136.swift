2ae817390975894a3c86b67a3ccd1a06f0436271
598069751@qq.com <598069751@qq.com>
2016-10-28 12:51:49 +0800

    day6注释

--- LeetCode/LeetCode/Questions/Day6_Single_Number_136/Day6_Single_Number_136.swift
+++ LeetCode/LeetCode/Questions/Day6_Single_Number_136/Day6_Single_Number_136.swift
@@ -10,7 +10,15 @@ import Foundation
 
 class Day6_Single_Number_136: NSObject {
 
-    /// 题目和解题思路见292. NimGame.md
+    /// 整数型数组中，每个元素均出现两次，除了一个元素例外，如何找出这个元素？能否设计一个线性时间的算法，且不需要额外的存储空间？
+    
+    /// nums = [1,1,2,2,3,3,4]
+    /// Return: 4
+    /// 解题思路:对所有数字进行异或运算
+    
+    /// - parameter nums: 整数数组
+    ///
+    /// - returns: 数组中唯一只出现了一次的数字
     class private func singleNumber(_ nums: [Int]) -> Int {
         var result : Int = 0
         for i in nums {
