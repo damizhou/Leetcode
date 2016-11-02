2bdaa220a3f18a8baf793571ac841885ebf87cc5
598069751@qq.com <598069751@qq.com>
2016-11-02 16:55:15 +0800

    Day8_AddDigits_258

--- LeetCode/LeetCode/ViewController.swift
+++ LeetCode/LeetCode/ViewController.swift
@@ -12,6 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        第八天
+        Day8_AddDigits_258.solution()
+
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func week1() {
 //        第一天
 //        Day1_TwoSum_1.solution()
 //        第二天
@@ -23,17 +36,9 @@ class ViewController: UIViewController {
 //        第五天
 //        Day5_NimGame_292.solution()
 //        第六天
-//        Day6_Single_Number_136.solution();
+//        Day6_Single_Number_136.solution()
 //        第七天
-        Day7_SumofTwoIntegers_371.solution();
-
-    }
-
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
-        // Dispose of any resources that can be recreated.
+//        Day7_SumofTwoIntegers_371.solution()
     }
 
 
